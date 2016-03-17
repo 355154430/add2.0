de594f650b85f22526ed987f690f1b137f3266eb
3355154430 <355154430@qq.com>
2016-03-17 22:30:26 +0800

    add
    3月17号，课上用xcode拖拽控件制作简单的加法计算

--- add/ViewController.swift
+++ add/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBAction func add(sender: UIButton) {
+        var a:Double!=0
+        var b:Double!=0
+        var c:Double!=0
+        if(!x.text!.isEmpty) {
+            a=(x.text! as NSString).doubleValue
+        }
+        
+        if(!y.text!.isEmpty) {
+            b=(y.text! as NSString).doubleValue
+        }
+        c=a+b
+        z.text="\(c)"
+    }
+    
+    
+    @IBOutlet weak var z: UITextField!
+    
+    @IBOutlet weak var x: UITextField!
+
+    @IBOutlet weak var y: UITextField!
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
